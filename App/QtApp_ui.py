d77861165bb237f2eb404c3a0ce41f61ffb3f3f4
javiman555 <javiman555@gmail.com>
2022-04-15 23:24:00 +0200

    Fixed anaconda environtment / Conected the altair plot to the PyQt interface (QtApp.py and App directory needs serious cleaning)

--- App/QtApp_ui.py
+++ App/QtApp_ui.py
@@ -2,11 +2,13 @@
 
 # Form implementation generated from reading ui file 'QtApp.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.12.3
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -15,14 +17,17 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(320, 310, 93, 28))
+        self.pushButton.setGeometry(QtCore.QRect(10, 120, 93, 28))
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(324, 140, 161, 91))
+        self.label.setGeometry(QtCore.QRect(20, 10, 161, 91))
         self.label.setObjectName("label")
+        self.graphWidget = PlotWidget(self.centralwidget)
+        self.graphWidget.setGeometry(QtCore.QRect(10, 370, 221, 191))
+        self.graphWidget.setObjectName("graphWidget")
         self.webEngineView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
-        self.webEngineView.setGeometry(QtCore.QRect(490, 100, 300, 200))
-        self.webEngineView.setUrl(QtCore.QUrl("https://stackoverflow.com/questions/31937203/qwebengineview-opening-local-html"))
+        self.webEngineView.setGeometry(QtCore.QRect(190, 20, 601, 361))
+        self.webEngineView.setUrl(QtCore.QUrl("https://stackoverflow.com/questions/60210035/how-to-render-altair-vega-in-a-pyqt-widget"))
         self.webEngineView.setObjectName("webEngineView")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -41,8 +46,9 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
         self.label.setText(_translate("MainWindow", "Label"))
-
 from PyQt5 import QtWebEngineWidgets
+from pyqtgraph import PlotWidget
+
 
 if __name__ == "__main__":
     import sys
@@ -52,4 +58,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
